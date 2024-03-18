623bd51427aef2c06b66a27d53f3fe3633d5e322
stimotius <147174427+stimotius@users.noreply.github.com>
2024-03-18 09:38:47 +0700

    init

--- app/src/main/java/com/ubaya/advweek4160421132/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/advweek4160421132/view/StudentDetailFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ubaya.advweek4160421132.R
 import com.ubaya.advweek4160421132.databinding.FragmentStudentDetailBinding
 import com.ubaya.advweek4160421132.databinding.FragmentStudentListBinding
+import com.ubaya.advweek4160421132.viewmodel.DetailViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
     private lateinit var binding: FragmentStudentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,5 +33,20 @@ class StudentDetailFragment : Fragment() {
         return binding.root
         // Inflate the layout for this fragment
         // return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            with(binding) {
+                txtID.setText(it.id)
+                txtName.setText(it.name)
+                txtBod.setText(it.dob)
+                txtPhone.setText(it.phone)
+            }
+        })
     }
 }
